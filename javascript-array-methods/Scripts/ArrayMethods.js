c05a8fdaaa37496249ffdfbe72a7e939c972269e
James Priest <james-priest@outlook.com>
2017-07-02 05:40:50 -0700

    Migrate working version
    * This is version 1.0.0
    * This version passes all tests
    * Uses arrays to manage code samples

---
+++ javascript-array-methods/Scripts/ArrayMethods.js
@@ -0,0 +1,223 @@
+﻿/// <reference path="_references.js" />
+
+var js = [];
+var evtHandler = ['concat', 'indexOf', 'join', 'lastIndexOf', 'pop', 'push', 'reverse', 'shift', 'slice', 'sort', 'splice', 'toString', 'unshift', 'valueOf'];
+
+js[0] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];"
+    + "var pizzaVegetableParts = ['pepper', 'onion'];"
+    + "var pizzaParts = pizzaMeatParts.concat(pizzaVegetableParts);";
+js[1] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];"
+    + "var baconIndex = pizzaMeatParts.indexOf('bacon');";
+js[2] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];"
+    + "var meatParts = pizzaMeatParts.join();";
+js[3] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon', 'ham', 'prosciutto'];"
+    + "var lastHamIndex = pizzaMeatParts.lastIndexOf('ham');";
+js[4] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];"
+    + "var lastItem = pizzaMeatParts.pop();";
+js[5] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];"
+    + "var newLength = pizzaMeatParts.push('prosciutto');";
+js[6] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon', 'prosciutto'];"
+    + "pizzaMeatParts.reverse();";
+js[7] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];"
+    + "var firstItem = pizzaMeatParts.shift();";
+js[8] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon', 'meatball', 'prosciutto'];"
+    + "var mySlice = pizzaMeatParts.slice(1,3);";
+js[9] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon', 'meatball', 'prosciutto'];"
+    + "pizzaMeatParts.sort();";
+js[10] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon', 'meatball', 'prosciutto'];"
+    + "var mySlice = pizzaMeatParts.splice(1,2,'spam');";
+js[11] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];"
+    + "var meatParts = pizzaMeatParts.toString();";
+js[12] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];"
+    + "var newLength = pizzaMeatParts.unshift('prosciutto');";
+js[13] = "var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];"
+    + "var meatParts = pizzaMeatParts.valueOf();";
+
+/*
+ * object to hold code
+ * method: concat
+ * variable: pizzaMeatParts
+ * expression: ['pepperoni', 'ham', 'bacon']
+ */
+//var sampleCode = [
+//    { 'method': 'concat', 'variable': 'pizzaMeatParts', 'expression': "['pepperoni', 'ham', 'bacon'];" },
+//    { 'method': 'concat', 'variable': 'pizzaVegetableParts', 'expression': "['pepper', 'onion'];" },
+//    { 'method': 'concat', 'variable': 'pizzaParts', 'expression': 'pizzaMeatParts.concat(pizzaVegetableParts);' },
+//    { 'method': 'indexOf', 'variable': 'pizzaMeatParts', 'expression': "['pepperoni', 'ham', 'bacon'];" },
+//    { 'method': 'indexOf', 'variable': 'baconIndex', 'expression': "pizzaMeatParts.indexOf('bacon');" }
+//]
+
+
+function initialize() {
+    for (var i = 0; i < js.length; i++) {
+        var codeElement = document.getElementById('code' + i);
+        addDisplayFormatting(codeElement, i);
+  
+        var btn = document.getElementById('btn-' + evtHandler[i]);
+        btn.addEventListener('click', eval(evtHandler[i] + 'Code'), false);
+    }
+}
+
+function addDisplayFormatting(codeElement, counter) {
+    codeElement.innerHTML = js[counter].replace(/;/g, ';\n');
+}
+
+function removeDisplayFormatting(codeSnippet) {
+    return codeSnippet.replace(/; /g, ';');
+}
+
+function formatResultsforDisplay(resultString,resultLocationDivID) {
+    var result = document.getElementById(resultLocationDivID);
+    result.className = "prettyprint";
+    result.innerHTML = resultString;
+    prettyPrint();
+}
+
+
+/* Method Handlers for click events */
+function concatCode(e) {
+    // run sample code
+    //var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];
+    //var pizzaVegetablesParts = ['pepper', 'onion'];
+    //var pizzaParts = pizzaMeatParts.concat(pizzaVegetablesParts);
+
+    eval(js[0]);
+
+    formatResultsforDisplay("pizzaParts == ['" + pizzaParts.join("', '") + "']", 'result0');
+}
+ 
+function indexOfCode() {
+    // run sample code
+    //var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];
+    //var baconIndex = pizzaMeatParts.indexOf('bacon');
+
+    eval(js[1]);
+
+    formatResultsforDisplay('baconIndex == ' + baconIndex.toString(), 'result1');
+}
+
+function joinCode() {
+    // run sample code
+    //var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];
+    //var meatParts = pizzaMeatParts.join();
+
+    eval(js[2]);
+
+    formatResultsforDisplay('meatParts == "' + meatParts.toString() + '"', 'result2');
+}
+
+function lastIndexOfCode() {
+    // run sample code
+    //var pizzaMeatParts = ['pepperoni', 'ham', 'bacon', 'ham', 'prosciutto'];
+    //var lastHamIndex = pizzaMeatParts.lastIndexOf('ham');
+
+    eval(js[3]);
+
+    formatResultsforDisplay('lastHamIndex == ' + lastHamIndex.toString(), 'result3');
+}
+
+function popCode() {
+    // run sample code
+    //var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];
+    //var lastItem = pizzaMeatParts.pop();
+
+    eval(js[4]);
+
+    var resultString = "pizzaMeatParts == ['pepperoni', 'ham']" + '\n';
+    resultString += 'lastItem == "' + lastItem.toString() + '"';
+    formatResultsforDisplay(resultString, 'result4');
+}
+
+function pushCode() {
+    // run sample code
+    //var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];
+    //var newLength = pizzaMeatParts.push('prosciutto');
+
+    eval(js[5]);
+
+    var resultString = "pizzaMeatParts == ['" + pizzaMeatParts.join("', '") + "']\n";
+    resultString += 'newLength == ' + newLength.toString();
+    formatResultsforDisplay(resultString, 'result5');
+}
+
+function reverseCode() {
+    // run sample code
+    //var pizzaMeatParts = ['pepperoni', 'ham', 'bacon', 'prosciutto'];
+    //pizzaMeatParts.reverse();
+
+    eval(js[6]);
+
+    var resultString = "pizzaMeatParts == ['" + pizzaMeatParts.join("', '") + "']";
+    formatResultsforDisplay(resultString, 'result6');
+}
+
+function shiftCode() {
+    // run sample code
+    //var pizzaMeatParts = ['pepperoni', 'ham', 'bacon'];
+    //var firstItem = pizzaMeatParts.shift();
+    var linesOfCode = js[7];
+
+    //runtimeExecuteJSCode(linesOfCode)();
+    var abc = runtimeExecuteJSCode(linesOfCode);
+    var myVar = abc();
+    //var result = myVar();
+    //var resultString = 'firstItem == "' + firstItem + '"';
+    var resultString = 'firstItem == "' + myVar + '"';
+    formatResultsforDisplay(resultString, 'result7');
+}
+
+var runtimeExecuteJSCode = function (linesOfCode) {
+    return function () {
+        eval(linesOfCode);
+        return firstItem;
+    }
+};
+//var runtimeExecuteJSCode = function (linesOfCode) {
+//    return function () {
+//        //eval(linesOfCode);
+//        return function () { eval(linesOfCode);};
+//    }
+//};
+function runtimeExecuteJSCodeFunc(linesOfCode) {
+    eval(linesOfCode);
+}
+
+ 
+function sliceCode() {
+    eval(js[8]);
+
+    var resultString = "mySlice == ['" + mySlice.join("', '") + "']";
+    formatResultsforDisplay(resultString, 'result8');
+}
+function sortCode() {
+    eval(js[9]);
+
+    var resultString = "pizzaMeatParts == ['" + pizzaMeatParts.join("', '") + "']";
+    formatResultsforDisplay(resultString, 'result9');
+}
+function spliceCode() {
+    eval(js[10]);
+
+    var resultString = "pizzaMeatParts == ['" + pizzaMeatParts.join("', '") + "']\n";
+    resultString += "mySlice == ['" + mySlice.join("', '") + "']";
+    formatResultsforDisplay(resultString, 'result10');
+}
+function toStringCode() {
+    eval(js[11]);
+
+    var resultString = 'meatParts == "' + meatParts.replace(/,/g, ', ') + '"';
+    formatResultsforDisplay(resultString, 'result11');
+}
+function unshiftCode() {
+    eval(js[12]);
+
+    var resultString = "pizzaMeatParts == ['" + pizzaMeatParts.join("', '") + "']\n";
+    resultString += "newLength == " + newLength.toString();
+    formatResultsforDisplay(resultString, 'result12');
+}
+function valueOfCode() {
+    eval(js[13]);
+
+    var resultString = "meatParts == ['" + meatParts.join("', '") + "']";
+    formatResultsforDisplay(resultString, 'result13');
+}
---
+++ javascript-array-methods/Scripts/tests.js
@@ -0,0 +1,255 @@
+﻿/// <reference path="_references.js" />
+
+module('JavaScript Array Test Suite', { setup: function () { initialize(); } });
+
+test("'concat' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-concat");
+    var result = removeDisplayFormatting(document.getElementById('code0').innerText.trim());
+    var expected = js[0].trim();
+    // assert
+    equal(result, expected, "Test that 'concat' sample code set in JavaScript matches string displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result0').innerText
+    expected = "pizzaParts == ['pepperoni', 'ham', 'bacon', 'pepper', 'onion']";
+    equal(result, expected, "Test that result of concat operation returns the expected result");
+});
+
+test("'indexOf' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-indexOf");
+    var result = removeDisplayFormatting(document.getElementById('code1').innerText.trim());
+    var expected = js[1].trim();
+    // assert
+    equal(result, expected, "Test that 'indexOf' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result1').innerText
+    expected = "baconIndex == 2";
+    equal(result, expected, "Test that result of indexOf operation returns the expected result");
+});
+
+test("'join' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-join");
+    var result = removeDisplayFormatting(document.getElementById('code2').innerText.trim());
+    var expected = js[2].trim();
+    // assert
+    equal(result, expected, "Test that 'join' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result2').innerText
+    expected = 'meatParts == "pepperoni,ham,bacon"';
+    equal(result, expected, "Test that result of join operation returns the expected result");
+});
+
+test("'lastIndexOf' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-lastIndexOf");
+    var result = removeDisplayFormatting(document.getElementById('code3').innerText.trim());
+    var expected = js[3].trim();
+    // assert
+    equal(result, expected, "Test that 'lastIndexOf' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result3').innerText
+    expected = 'lastHamIndex == 3';
+    equal(result, expected, "Test that result of lastIndexOf operation returns the expected result");
+});
+
+test("'pop' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-pop");
+    var result = removeDisplayFormatting(document.getElementById('code4').innerText.trim());
+    var expected = js[4].trim();
+    // assert
+    equal(result, expected, "Test that 'pop' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result4').innerText
+    expected = "pizzaMeatParts == ['pepperoni', 'ham'] lastItem == \"bacon\"";
+    equal(result, expected, "Test that result of pop operation returns the expected result");
+});
+
+test("'push' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-push");
+    var result = removeDisplayFormatting(document.getElementById('code5').innerText.trim());
+    var expected = js[5].trim();
+    // assert
+    equal(result, expected, "Test that 'push' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result5').innerText
+    expected = "pizzaMeatParts == ['pepperoni', 'ham', 'bacon', 'prosciutto'] newLength == 4";
+    equal(result, expected, "Test that result of push operation returns the expected result");
+});
+
+test("'reverse' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-reverse");
+    var result = removeDisplayFormatting(document.getElementById('code6').innerText.trim());
+    var expected = js[6].trim();
+    // assert
+    equal(result, expected, "Test that 'reverse' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result6').innerText
+    expected = "pizzaMeatParts == ['prosciutto', 'bacon', 'ham', 'pepperoni']";
+    equal(result, expected, "Test that result of reverse operation returns the expected result");
+});
+
+test("'shift' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-shift");
+    var result = removeDisplayFormatting(document.getElementById('code7').innerText.trim());
+    var expected = js[7].trim();
+    // assert
+    equal(result, expected, "Test that 'shift' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result7').innerText
+    expected = "firstItem == \"pepperoni\"";
+    equal(result, expected, "Test that result of shift operation returns the expected result");
+});
+
+test("'slice' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-slice");
+    var result = removeDisplayFormatting(document.getElementById('code8').innerText.trim());
+    var expected = js[8].trim();
+    // assert
+    equal(result, expected, "Test that 'slice' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result8').innerText
+    expected = "mySlice == ['ham', 'bacon']";
+    equal(result, expected, "Test that result of slice operation returns the expected result");
+});
+
+test("'sort' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-sort");
+    var result = removeDisplayFormatting(document.getElementById('code9').innerText.trim());
+    var expected = js[9].trim();
+    // assert
+    equal(result, expected, "Test that 'sort' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result9').innerText
+    expected = "pizzaMeatParts == ['bacon', 'ham', 'meatball', 'pepperoni', 'prosciutto']";
+    equal(result, expected, "Test that result of sort operation returns the expected result");
+});
+
+test("'splice' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-splice");
+    var result = removeDisplayFormatting(document.getElementById('code10').innerText.trim());
+    var expected = js[10].trim();
+    // assert
+    equal(result, expected, "Test that 'splice' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result10').innerText
+    expected = "pizzaMeatParts == ['pepperoni', 'spam', 'meatball', 'prosciutto'] mySlice == ['ham', 'bacon']";
+    equal(result, expected, "Test that result of splice operation returns the expected result");
+});
+
+test("'toString' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-toString");
+    var result = removeDisplayFormatting(document.getElementById('code11').innerText.trim());
+    var expected = js[11].trim();
+    // assert
+    equal(result, expected, "Test that 'toString' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result11').innerText
+    expected = 'meatParts == "pepperoni, ham, bacon"';
+    equal(result, expected, "Test that result of toString operation returns the expected result");
+});
+
+test("'unshift' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-unshift");
+    var result = removeDisplayFormatting(document.getElementById('code12').innerText.trim());
+    var expected = js[12].trim();
+    // assert
+    equal(result, expected, "Test that 'unshift' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result12').innerText
+    expected = "pizzaMeatParts == ['prosciutto', 'pepperoni', 'ham', 'bacon'] newLength == 4";
+    equal(result, expected, "Test that result of unshift operation returns the expected result");
+});
+
+test("'valueOf' method", function () {
+    expect(2);
+
+    // arrange
+    var btn = document.getElementById("btn-valueOf");
+    var result = removeDisplayFormatting(document.getElementById('code13').innerText.trim());
+    var expected = js[13].trim();
+    // assert
+    equal(result, expected, "Test that 'valueOf' sample code set in JS matches sample code displayed in browser.");
+
+    // act
+    QUnit.triggerEvent(btn, 'click');
+    // assert
+    result = document.getElementById('result13').innerText
+    expected = "meatParts == ['pepperoni', 'ham', 'bacon']";
+    equal(result, expected, "Test that result of valueOf operation returns the expected result");
+});
